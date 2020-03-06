b457225b05dd991f3415b8369c65ee39eb5bf4ef
Lukas K <50622496+lulu12329@users.noreply.github.com>
2020-03-06 11:39:10 +0100

    Add files via upload
    finished level1

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,60 @@
+import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-	static private int n;	//Anzahl der Parkplätze
+	static private int n;	//Anzahl der Parkplï¿½tze
 	static private int m;	//Anzahl der Autos die heute kommen
 	static private ArrayList<Integer> cars = new ArrayList<Integer>();
-	
+
 	private static void readIn(String path) throws FileNotFoundException {
 		File input = new File(path);
 		Scanner r = new Scanner(input);
-		
+
 		String line1 = r.nextLine();
 		String line2 = r.nextLine();
-		
+
 		String[] nm = line1.split(" ");
-		String[] einträge = line2.split(" ");
-		
+		String[] eintraege = line2.split(" ");
+
 		n = Integer.parseInt(nm[0]);
 		m = Integer.parseInt(nm[1]);
-		
-		for (String eintrag : einträge) {
+
+		for (String eintrag : eintraege) {
 			cars.add(Integer.parseInt(eintrag));
 		}
-				
-				
+
+
 		System.out.println(line1 + "\n" + line2 + "\n");
 		System.out.println(n +" "+ m +" " + " " + cars.get(2));
-		
+
 		r.close();
 	}
-	
+
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner s = new Scanner(System.in);
-		System.out.print("Bite Path zum input eingeben: ");
+		System.out.print("Bitte Path zum input eingeben: ");
 		String path = s.nextLine();
-		
+
 		readIn(path);
+		System.out.println(count(cars));
 	}
 
+	private static int count(ArrayList<Integer> cars){
+		int maxCars=0;
+		int counter=0;
+		for(Integer i:cars){
+			if(i>0){
+				counter++;
+				if(counter>maxCars){
+					maxCars=counter;
+				}
+			}else if(i<0){
+				counter--;
+			}
+		}
+		return maxCars;
+	}
 }
